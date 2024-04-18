b80aac495f7bec0cd24ec683a34780f756416a6d
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-18 15:57:07 -0300

    Add project files.

---
+++ EstruturaDeRepeticao/Program.cs
@@ -0,0 +1,54 @@
+﻿//Ler um número e ler quantas vezes será incrementado em cima desse número
+
+int n1 = 0, max = 0;
+int contador = 0;
+int aux;
+
+
+Console.WriteLine("Digite um número: ");
+n1 = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("\nDigite o número incrementador: ");
+max = int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("\nAqui é For");
+
+aux = n1;
+for (int i = 0; i <= max; i++)
+{
+    Console.WriteLine(aux);
+    aux++;
+    
+
+}
+
+
+Console.WriteLine("\nAqui é o Do-while");
+
+aux = n1;
+do
+{
+    Console.WriteLine(aux);
+    aux++;
+    contador++;
+
+} while (contador <= max);
+
+
+
+Console.WriteLine("\nAqui é o While");
+aux = n1;
+
+contador = 0;
+while (contador <= max) 
+{
+    
+    Console.WriteLine(aux);
+    aux++;
+    contador++;
+
+}
